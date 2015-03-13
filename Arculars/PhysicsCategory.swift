10100e64c68b57d92ac69e811ba3216b966186e4
Roman Blum <roman1blum@gmail.com>
2015-03-13 13:12:57 +0100

    outsourced physicscategory

---
+++ Arculars/PhysicsCategory.swift
@@ -0,0 +1,15 @@
+//
+//  PhysicsCategory.swift
+//  Arculars
+//
+//  Created by Roman Blum on 13/03/15.
+//  Copyright (c) 2015 RMNBLM. All rights reserved.
+//
+
+import Foundation
+
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let Ball      : UInt32 = 0b1
+    static let Arc       : UInt32 = 0b10
+}
