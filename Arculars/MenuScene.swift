f6624c68210cac59faea41b9dd2c06b223d6d4ae
Roman Blum <roman1blum@gmail.com>
2015-03-14 17:06:40 +0100

    added menuscene

---
+++ Arculars/MenuScene.swift
@@ -0,0 +1,14 @@
+//
+//  MenuScene.swift
+//  Arculars
+//
+//  Created by Roman Blum on 14/03/15.
+//  Copyright (c) 2015 RMNBLM. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+}
