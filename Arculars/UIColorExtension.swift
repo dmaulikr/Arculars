db331baaa580e923d8b6d80409ec330551acf1fd
Roman Blum <roman1blum@gmail.com>
2015-03-15 10:44:25 +0100

    new extension methods for UIColor

--- Arculars/UIColorExtension.swift
+++ Arculars/UIColorExtension.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 extension UIColor {
+    /**
+    Construct a UIColor using an HTML/CSS RGBA formatted value
+    
+    :param: rgba value in String format
+    */
     convenience init(rgba: String) {
         var red:   CGFloat = 0.0
         var green: CGFloat = 0.0
@@ -50,5 +55,44 @@ extension UIColor {
             print("Invalid RGB string, missing '#' as prefix")
         }
         self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    /**
+    Return a modified color using the brightness factor provided
+    
+    :param: factor brightness factor
+    :returns: modified color
+    */
+    func colorWithBrightnessFactor(factor: CGFloat) -> UIColor {
+        var hue : CGFloat = 0
+        var saturation : CGFloat = 0
+        var brightness : CGFloat = 0
+        var alpha : CGFloat = 0
+        
+        if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+            return UIColor(hue: hue, saturation: saturation, brightness: brightness * factor, alpha: alpha)
+        } else {
+            return self;
+        }
+    }
+    
+    /**
+    Returns a lighter color by the provided percentage
+    
+    :param: lighting percent percentage
+    :returns: lighter UIColor
+    */
+    func lighterColor(percent : Double) -> UIColor {
+        return colorWithBrightnessFactor(CGFloat(1 + percent));
+    }
+    
+    /**
+    Returns a darker color by the provided percentage
+    
+    :param: darking percent percentage
+    :returns: darker UIColor
+    */
+    func darkerColor(percent : Double) -> UIColor {
+        return colorWithBrightnessFactor(CGFloat(1 - percent));
     }
 }
