22d46c1817a83f301876a73d9a0c39ce056c0547
Roman Blum <roman1blum@gmail.com>
2015-03-15 10:44:39 +0100

    changed color names

--- Arculars/Colors.swift
+++ Arculars/Colors.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 struct Colors {
-    static var LightFontColor : UIColor = UIColor(rgba: "#9B9B9B")
-    static var LightBackground : UIColor = UIColor(rgba: "#F3F3F3")
-    static var LightBlue : UIColor = UIColor(rgba: "#4A90E2")
-    static var LightOrange : UIColor = UIColor(rgba: "#F5A623")
-    static var LightRed : UIColor = UIColor(rgba: "#FF5460")
+    static var FontColor : UIColor = UIColor(rgba: "#9B9B9B")
+    static var Background : UIColor = UIColor(rgba: "#F3F3F3")
     
-    static func randomLightBall() -> UIColor {
+    static var LightBlue : UIColor = UIColor(rgba: "#CFE5FF")
+    static var Blue : UIColor = UIColor(rgba: "#4A90E2")
+    static var LightOrange : UIColor = UIColor(rgba: "#FFE9C5")
+    static var Orange : UIColor = UIColor(rgba: "#F5A623")
+    static var LightRed : UIColor = UIColor(rgba: "#FFD9DC")
+    static var Red : UIColor = UIColor(rgba: "#FF5460")
+    
+    static func randomBallColor() -> UIColor {
         let colors = [
-            LightBlue,
-            LightOrange,
-            LightRed
+            Blue,
+            Orange,
+            Red
         ]
         var random = Int(arc4random_uniform(UInt32(colors.count)));
         return colors[random]
