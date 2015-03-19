ff424e788d403e8fa9ee3fe58dd8fcfdf7beb6ec
Roman Blum <roman1blum@gmail.com>
2015-03-19 14:19:18 +0100

    added facebook and twitter color

--- Arculars/Colors.swift
+++ Arculars/Colors.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 struct Colors {
-    static var FontColor : UIColor = UIColor(rgba: "#9B9B9B")
-    static var Background : UIColor = UIColor(rgba: "#F3F3F3")
+    static let FontColor : UIColor = UIColor(rgba: "#9B9B9B")
+    static let Background : UIColor = UIColor(rgba: "#F3F3F3")
     
-    static var LightBlue : UIColor = UIColor(rgba: "#CFE5FF")
-    static var Blue : UIColor = UIColor(rgba: "#4A90E2")
-    static var LightOrange : UIColor = UIColor(rgba: "#FFE9C5")
-    static var Orange : UIColor = UIColor(rgba: "#F5A623")
-    static var LightRed : UIColor = UIColor(rgba: "#FFD9DC")
-    static var Red : UIColor = UIColor(rgba: "#FF5460")
+    static let LightBlue : UIColor = UIColor(rgba: "#CFE5FF")
+    static let Blue : UIColor = UIColor(rgba: "#4A90E2")
+    static let LightOrange : UIColor = UIColor(rgba: "#FFE9C5")
+    static let Orange : UIColor = UIColor(rgba: "#F5A623")
+    static let LightRed : UIColor = UIColor(rgba: "#FFD9DC")
+    static let Red : UIColor = UIColor(rgba: "#FF5460")
     
-    static var FacebookBlue : UIColor = UIColor(rgba: "#6274A5")
-    static var TwitterBlue : UIColor = UIColor(rgba: "#6EC7E3")
+    static let FacebookBlue : UIColor = UIColor(rgba: "#6274A5")
+    static let TwitterBlue : UIColor = UIColor(rgba: "#6EC7E3")
     
     static func randomBallColor() -> UIColor {
         let colors = [
