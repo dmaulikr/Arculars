7eb56c5218b531e7084b06e5436704a76b9c2cb3
Roman Blum <roman1blum@gmail.com>
2015-03-19 21:21:41 +0100

    added settings scene

---
+++ Arculars/SettingsScene.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsScene.swift
+//  Arculars
+//
+//  Created by Roman Blum on 19/03/15.
+//  Copyright (c) 2015 RMNBLM. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SettingsScene: SKScene {
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToView(view: SKView) {
+        
+    }
+}
