466d251347cd0a0ea4e46b3923727f6be1b700ad
Roman Blum <roman1blum@gmail.com>
2015-03-24 14:53:10 +0100

    first draft of webpage files

---
+++ ArcularsWeb.io/js/fadein.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $(".logo").addClass("animated fadeInUp visible");
+    setTimeout(function(){
+        $("h1").addClass("animated fadeInUp visible");
+    }, 500);
+    setTimeout(function(){
+        $("h2").addClass("animated fadeInUp visible");
+    }, 1000);
+    setTimeout(function(){
+        $(".section0 .app-store-btn").addClass("animated fadeInUp visible");
+    }, 1500);
+});
