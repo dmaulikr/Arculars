b6e70e96de099a65bb488ab60f211d4dfe80f6cb
Roman Blum <roman1blum@gmail.com>
2015-03-28 11:24:23 +0100

    added get and set methods for difficulty

--- Arculars/SettingsHandler.swift
+++ Arculars/SettingsHandler.swift
@@ -10,6 +10,7 @@ import Foundation
 
 let SETTINGS_VIBRATION          = "settings_vibration"
 let SETTINGS_SOUND              = "settings_sound"
+let SETTINGS_DIFFICULTY         = "settings_difficulty"
 
 class SettingsHandler {
     
@@ -31,5 +32,28 @@ class SettingsHandler {
     
     class func getSoundSetting() -> Bool {
         return NSUserDefaults.standardUserDefaults().boolForKey(SETTINGS_SOUND)
+    }
+    
+    class func toggleDifficulty() {
+        var difficulty = getDifficulty()
+        switch difficulty {
+        case .Easy:
+            NSUserDefaults.standardUserDefaults().setInteger(Difficulty.Normal.rawValue, forKey: SETTINGS_DIFFICULTY)
+            break
+        case .Normal:
+            NSUserDefaults.standardUserDefaults().setInteger(Difficulty.Hard.rawValue, forKey: SETTINGS_DIFFICULTY)
+            break
+        case .Hard:
+            NSUserDefaults.standardUserDefaults().setInteger(Difficulty.Easy.rawValue, forKey: SETTINGS_DIFFICULTY)
+            break
+        }
+    }
+    
+    class func getDifficulty() -> Difficulty {
+        var difficulty = NSUserDefaults.standardUserDefaults().integerForKey(SETTINGS_DIFFICULTY)
+        if Difficulty(rawValue: difficulty) == nil {
+            return Difficulty.Normal
+        }
+        return Difficulty(rawValue: difficulty)!
     }
 }
