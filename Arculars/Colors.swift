474bf4e39d5317e08671b1952cb26cf6e719e9de
Roman Blum <roman1blum@gmail.com>
2015-04-06 16:06:43 +0200

    added new colors

--- Arculars/Colors.swift
+++ Arculars/Colors.swift
@@ -11,11 +11,13 @@ import UIKit
 struct Colors {
     static let ScoreColor       = UIColor(rgba: "#283233")
     static let FontColor        = UIColor(rgba: "#F0F0F0")
+    static let DisabledColor    = UIColor.grayColor()
     static let Background       = UIColor(rgba: "#121F21")
     static let AppColorOne      = UIColor(rgba: "#6D21CD")
     static let AppColorTwo      = UIColor(rgba: "#E022BF")
     static let AppColorThree    = UIColor(rgba: "#39E0BA")
     static let AppColorFour     = UIColor(rgba: "#2DFB7D")
+    static let PowerupColor     = UIColor(rgba: "#FCDA0A")
     
     static let WhatsAppGreen    = UIColor(rgba: "#71BA5B")
     static let FacebookBlue     = UIColor(rgba: "#6274A5")
