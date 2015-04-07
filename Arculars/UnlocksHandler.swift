58de0b7203d354d4293adda71bedcf6ce6e20c1c
Roman Blum <roman1blum@gmail.com>
2015-04-07 22:09:42 +0200

    added unlockshandler

---
+++ Arculars/UnlocksHandler.swift
@@ -0,0 +1,27 @@
+//
+//  UnlocksHandler.swift
+//  Arculars
+//
+//  Created by Roman Blum on 07/04/15.
+//  Copyright (c) 2015 RMNBLM. All rights reserved.
+//
+
+import Foundation
+
+let CREDIS                = "credits"
+
+class UnlocksHandler {
+    
+    class func getCredits() -> UInt32 {
+        return 0
+    }
+    
+    class func depositCredits(credits: UInt32) {
+        
+    }
+    
+    class func withdrawCredits(credits: UInt32) {
+        
+    }
+    
+}
