1b1f68893dde3c48086dcc03913156ea325bb191
Roman Blum <roman1blum@gmail.com>
2015-04-17 07:08:54 +0200

    update text

--- Arculars/Strings.swift
+++ Arculars/Strings.swift
@@ -10,7 +10,7 @@ struct Strings {
     static let ArcularsEmail      = "arculars@rmnblm.io"
     static let ArcularsWebsite    = "http://arculars.rmnblm.io"
     static let ArcularsAppStore   = "http://appstore.com/arculars"
-    static let SharingText        = "Check out my score in #ARCULARS! Download on " + ArcularsAppStore
+    static let SharingText        = "OMG! Check out my score in #ARCULARS! Download on " + ArcularsAppStore
     
     static let LeaderboardEndless = "leaderboard.endless"
     static let LeaderboardTimed   = "leaderboard.timed"
