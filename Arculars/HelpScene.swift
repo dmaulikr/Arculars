494c37bfde4254b772c53262fb2d79fdc5b74e38
Roman Blum <roman1blum@gmail.com>
2015-04-20 00:28:26 +0200

    update help scene

--- Arculars/HelpScene.swift
+++ Arculars/HelpScene.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import SpriteKit
+import AudioToolbox
 
-class HelpScene: SKScene {
+class HelpScene: SKScene, SKPhysicsContactDelegate {
     
     // MARK: - VARIABLE DECLARATIONS
     weak var sceneDelegate : SceneDelegate?
     
     private var rootNode = SKNode()
+    private var nextBall : Ball?
+    private var score : Score!
     
     private var btnClose : SKShapeNode!
     
@@ -32,6 +35,10 @@ class HelpScene: SKScene {
         
         // Add Root Node
         addChild(rootNode)
+        
+        // Setup Scene Physics
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVectorMake(0, 0)
         
         initScene()
         
@@ -53,12 +60,35 @@ class HelpScene: SKScene {
         // INIT TITLE
         rootNode.addChild(Nodes.getSceneTitle(frame.size, content: "HOW TO PLAY"))
         
-        // TUTORIAL IMAGE
-        var image = SKSpriteNode(imageNamed: "image-tutorial")
-        var aspectRatio = image.size.width / image.size.height
-        image.size = CGSize(width: size.width, height: size.width / aspectRatio)
-        image.position = CGPoint(x: 0, y: 0)
-        rootNode.addChild(image)
+        // SAMPE GAMESCENE
+        var radius = size.height / 24
+        var thickness = size.height / 40
+        var c1 = Circle(position: CGPoint(x: 0, y: (size.height / 4)), radius: radius, thickness: thickness, clockwise: false, pointsPerHit: 2, color: ThemeHandler.Instance.getCurrentColors().AppColorOne)
+        c1.setColor(ThemeHandler.Instance.getCurrentColors().AppColorOne)
+        c1.setSpeed(2.0, max: 2.0)
+        c1.fadeIn()
+        rootNode.addChild(c1)
+        
+        var c2 = Circle(position: CGPoint(x: 0, y: (size.height / 4)), radius: radius + thickness, thickness: thickness, clockwise: true, pointsPerHit: 1, color: ThemeHandler.Instance.getCurrentColors().AppColorThree)
+        c2.setColor(ThemeHandler.Instance.getCurrentColors().AppColorThree)
+        c2.setSpeed(1.6, max: 1.6)
+        c2.fadeIn()
+        rootNode.addChild(c2)
+        
+        score = Score(position: CGPoint(x: 0, y: (size.height / 4) + radius + (3 * thickness)))
+        score.fontSize = size.height / 40
+        rootNode.addChild(score)
+        
+        addBall()
+        var wait = SKAction.waitForDuration(3.0)
+        var shoot = SKAction.runBlock({()
+            self.shootBall()
+            self.addBall()
+        })
+        runAction(SKAction.repeatActionForever(SKAction.sequence([shoot, wait])))
+        
+        // TEXT
+        
         
         // INIT CLOSE BUTTON
         btnClose = Nodes.getTextButton(frame.size, content: "CLOSE")
@@ -76,4 +106,39 @@ class HelpScene: SKScene {
             }
         }
     }
+    
+    // MARK: - CONTACTS DELEGATE
+    func didBeginContact(contact: SKPhysicsContact) {
+        var ball : Ball
+        var circle : Circle
+        
+        if contact.bodyA.categoryBitMask == PhysicsCategory.ball.rawValue {
+            ball = contact.bodyA.node as! Ball
+            circle = contact.bodyB.node?.parent as! Circle // parent because the arc's parent
+        } else {
+            ball = contact.bodyB.node as! Ball
+            circle = contact.bodyA.node?.parent as! Circle // parent because the arc's parent
+        }
+        
+        ball.physicsBody!.categoryBitMask = PhysicsCategory.none.rawValue
+        ball.removeFromParent()
+        
+        if (ball.nodeColor == circle.nodeColor) {
+            runAction(SKAction.playSoundFileNamed("hit1.wav", waitForCompletion: false))
+            score.increaseByWithColor(circle.pointsPerHit, color: circle.nodeColor)
+        } else {
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+        }
+    }
+    
+    // MARK: - HELPER FUNCTIONS
+    private func addBall() {
+        nextBall = Ball(color: ThemeHandler.Instance.getCurrentColors().AppColorOne, position: CGPoint(x: 0, y: 0), radius: size.height / 80)
+        rootNode.addChild(nextBall!.fadeIn())
+    }
+    
+    private func shootBall() {
+        nextBall?.shoot(size.height)
+    }
+    
 }
